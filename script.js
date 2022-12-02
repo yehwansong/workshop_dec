ac2dae77414ca3a5bf881f913822b3671efc0182
yehwansong <song-yehwan@MacBook-Pro-5.local>
2022-12-02 10:52:09 +0100

    ad

--- script.js
+++ script.js
@@ -40,14 +40,38 @@ scene.add( sphere );
 
 //cube with texture
 let loader = new THREE.TextureLoader();
-var texture = loader.load( 'img/cha-01.png' );
-	texture.wrapS = THREE.RepeatWrapping;
-	texture.wrapT = THREE.RepeatWrapping;
-	texture.repeat.set(1,1);
+var texture_2 = loader.load( 'img/cha-01.png' );
+	texture_2.wrapS = THREE.RepeatWrapping;
+	texture_2.wrapT = THREE.RepeatWrapping;
+	texture_2.repeat.set(1,1);
 var geometry_2 = new THREE.BoxGeometry(1, 1, 1);
-var material_2 = new THREE.MeshBasicMaterial({ color: 0x2196f3, map:texture, side: THREE.DoubleSide });
+var material_2 = new THREE.MeshBasicMaterial({ color: 0x2196f3, map:texture_2, side: THREE.DoubleSide });
 var cube_2     = new THREE.Mesh(geometry_2, material_2);
 scene.add(cube_2);
+
+
+
+//keyboard input
+var texture_3 = loader.load( 'img/cha-02.png' );
+	texture_3.wrapS = THREE.RepeatWrapping;
+	texture_3.wrapT = THREE.RepeatWrapping;
+	texture_3.repeat.set(1,1);
+var geometry_3 = new THREE.BoxGeometry(1, 1, 1);
+var material_3 = new THREE.MeshBasicMaterial({ color: 0x2196f3, map:texture_3, side: THREE.DoubleSide });
+var cube_3     = new THREE.Mesh(geometry_3, material_3);
+cube_3.position.y = -2
+document.addEventListener('keydown',press)
+function press(e){
+	if (e.keyCode === 87 /* w */ ){
+		scene.add(cube_3);
+	}
+}
+
+
+
+
+
+
 
 
 
