7d07987160db9768905470a02f13bd97eee22201
yehwansong <song-yehwan@MacBook-Pro-5.local>
2022-12-05 08:51:57 +0100

    add

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ scene.add( camera );
 
 
 //-----------------------controls-----------------------
-let controls = new THREE.OrbitControls(camera);
-controls.update();
+// let controls = new THREE.OrbitControls(camera);
+// controls.update();
 
 
 //-----------------------mesh-----------------------
@@ -91,6 +91,7 @@ var texture_2 = loader.load( 'img/cha-01.png' );
 var geometry_2 = new THREE.BoxGeometry(50, 50, 50);
 var material_2 = new THREE.MeshPhongMaterial({ color: 0x2196f3, map:texture_2, side: THREE.DoubleSide, opacity:0.5 });
 var cube_2     = new THREE.Mesh(geometry_2, material_2);
+    // cube_2.castShadow = true;
 cube_2.position.x = -50
 // scene.add(cube_2);
 
@@ -99,12 +100,18 @@ var texture = loader.load( 'img/cha-00.png' );
 texture.wrapS = THREE.RepeatWrapping;
 texture.wrapT = THREE.RepeatWrapping;
 texture.repeat.set(10, 10);
-const plane = new THREE.Mesh( new THREE.PlaneGeometry( 500, 500 ), new THREE.MeshBasicMaterial( {map:texture, side: THREE.DoubleSide} ) );
-    plane.rotation.x=degrees_to_radians(90)
-    // plane.position.x = final_length_val/2 - 0.5
-    plane.position.x = 0.5
-    plane.position.y = -1.5
-    // scene.add(plane)
+// const plane = new THREE.Mesh( new THREE.PlaneGeometry( 500, 500 ), new THREE.MeshBasicMaterial( {map:texture, side: THREE.DoubleSide} ) );
+//     plane.rotation.x=degrees_to_radians(90)
+//     plane.position.x = final_length_val/2 - 0.5
+//     plane.position.x = 0.5
+//     plane.position.y = -1.5
+//     scene.add(plane)
+
+// const plane = new THREE.Mesh(new THREE.PlaneGeometry(1200, 1200), new THREE.MeshStandardMaterial({color: 0xcccccc, side: THREE.DoubleSide}));
+//     plane.receiveShadow = true;
+//     plane.rotation.x=degrees_to_radians(90)
+//     scene.add(plane)
+
 
 function degrees_to_radians(degrees){
     var pi = Math.PI;
@@ -145,15 +152,20 @@ fontloader.load("https://s3-us-west-2.amazonaws.com/s.cdpn.io/254249/helvetiker_
 
 
 //-----------------------light-----------------------
-const color = 0xFF0000;
-const pointLight = new THREE.PointLight(color);
-pointLight.position.set(0, 300, 200);
-scene.add(pointLight);
+// const color = 0xFF0000;
+// const pointLight = new THREE.PointLight(color);
+// pointLight.position.set(0, 300, 200);
+// scene.add(pointLight);
 
 // const color = 0xFF0000;
 // const intensity = 0.5;
 // const light = new THREE.AmbientLight(color, intensity);
 // scene.add(light);
+
+const directionalLight = new THREE.SpotLight(0xffffff, 1, 0, Math.PI / 4, 1);
+directionalLight.castShadow = true;
+directionalLight.position.set(0, 100, 80);
+scene.add(directionalLight);
 
 
 
@@ -184,7 +196,7 @@ scene.add(pointLight);
 var time = 0
 
 function onUpdate() {
-    time++
+    // time++
     // if(time<1000){
     //     camera.position.y = animation_calucator(time, 0, 1000, 0, -100)
     // }else if(time<2000){
@@ -193,9 +205,16 @@ function onUpdate() {
 
 }
 function animation_calucator(time, start, end, value_1, value_2) {
-        return value_1 + (value_2 - value_1) * (time - start) / (end - start);
+    modified_time = easeInOutCubic((time-start)/(end - start))*(end - start)+start
+    return value_1 + (value_2 - value_1) * (modified_time - start) / (end - start);
 }
-
+//https://easings.net/
+function easeOutCubic(x){
+    return 1 - Math.pow(1 - x, 3);
+}
+function easeInOutCubic(x) {
+    return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+}
 
 
 
@@ -227,6 +246,8 @@ function press(e){
 var renderer = new THREE.WebGLRenderer({antialias:1, alpha: 1});
 renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.getElementById('main').appendChild( renderer.domElement );
 
 
